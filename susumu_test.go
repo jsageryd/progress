f7e9a5b08ca59bed530991173c1bb11201b25bf9
Johan Sageryd <j@1616.se>
2016-04-05 14:52:05 +0200

    Update examples to use package from the outside
    This better shows actual use.

--- susumu_test.go
+++ susumu_test.go
@@ -1,13 +1,14 @@
-package susumu
+package susumu_test
 
 import (
 	"fmt"
+	"github.com/jsageryd/susumu"
 	"time"
 )
 
 // This example shows a single progress bar rendering.
 func ExampleBar_Draw() {
-	b := &Bar{Width: 80, Max: 256, Position: 192, Unit: "KiB"}
+	b := &susumu.Bar{Width: 80, Max: 256, Position: 192, Unit: "KiB"}
 	err := b.Draw()
 	if err != nil {
 		panic(err)
@@ -19,7 +20,7 @@ func ExampleBar_Draw() {
 
 // This is an example of an animated progress bar.
 func ExampleBar_Draw_second() {
-	b := &Bar{Width: 80, Max: 256, Position: 0, Unit: "KiB"}
+	b := &susumu.Bar{Width: 80, Max: 256, Position: 0, Unit: "KiB"}
 	ticker := time.Tick(20 * time.Millisecond)
 	for b.Position < b.Max {
 		<-ticker
